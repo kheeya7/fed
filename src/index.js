88f2389c8fe2f782c87c1ff5aeca4aec80c80b55
Kate Sohng <kheeya7@hotmail.com>
2019-01-13 14:50:45 -0800

    essential modifications with LESS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import 'bootstrap'
-// import './styles/bootstrap.min.css'
-// import './styles/bootstrap-grid.min.css'
-// import './styles/bootstrap-reboot.min.css'
-// import './styles/bootstrap-theme.min.css'
-// import './style.css'
 import './scss/app.scss'
+import './style.less'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
 
 ReactDOM.render(
   <div />,
