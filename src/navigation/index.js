4ba1508624a2519e1c624b7f8ba525685ca3fc24
Younghoon Gim <khunny7@live.com>
2019-05-21 21:02:40 -0700

    sticky header

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,68 +1,86 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default class NavView extends React.PureComponent {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            hasScrolled: false,
+        };
+    }
+
+    componentDidMount() {
+        document.onscroll = () => {
+            if (window.pageYOffset > 100) {
+                this.setState({
+                    hasScrolled: true,
+                });
+            } else {
+                this.setState({
+                    hasScrolled: false,
+                });
+            }
+        };
+    }
+
     render() {
+        const navbarClassName = this.state.hasScrolled ? 'nav-view-container navbar-scrolled' : 'nav-view-container';
+
         return (
-            <div className="background">
-                <Container className="nav-view-container" role="banner">
-                    <div className="nav-wrapper">
-                        <nav className="navbar bg-bright navbar-bright navbar-expand-md no-hor-padding">
-                            <a
-                                className="navbar-brand d-sm-inline-block font-weight-bold"
-                                href="/">
-                                <div className="logo">
-                                    <h1>Kate Sohng</h1>
-                                    <h2 className="navbar-text">
-                                        Front-End Developer | UX Designer
+            <Navbar className={navbarClassName} fixed="top" expand="md" collapseOnSelect>
+                <a
+                    className="navbar-brand d-sm-inline-block font-weight-bold"
+                    href="/">
+                    <div className="logo">
+                        <h1>Kate Sohng</h1>
+                        <h2 className="navbar-text">
+                            Front-End Developer | UX Designer
                             </h2>
-                                </div>
-                            </a>
-                            <button
-                                className="navbar-toggler"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#togglerNav"
-                                aria-controls="togglerNav"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation">
-                                <i
-                                    className="fa fa-bars"
-                                    aria-hidden="true">
-                                </i>
-                            </button>
-                            <div
-                                className="collapse navbar-collapse"
-                                id="togglerNav">
-                                <div className="navbar-nav ml-auto font-weight-bold">
-                                    <Link
-                                        to="/"
-                                        className="nav-item nav-link">
-                                        Home
-                                </Link>
-                                    <Link
-                                        to="/about"
-                                        className="nav-item nav-link">
-                                        About
-                                </Link>
-                                    <a
-                                        className="nav-item nav-link"
-                                        href="http://katesohng.com/blog"
-                                        target="_blank">
-                                        Blog
-                                </a>
-                                    <a
-                                        className="nav-item nav-link"
-                                        href="mailto:kheeya7@hotmail.com">
-                                        Contact
-                                </a>
-                                </div>
-                            </div>
-                        </nav>
                     </div>
-                </Container>
-            </div>
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#togglerNav"
+                    aria-controls="togglerNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <i
+                        className="fa fa-bars"
+                        aria-hidden="true">
+                    </i>
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="togglerNav">
+                    <div className="navbar-nav ml-auto font-weight-bold">
+                        <Link
+                            to="/"
+                            className="nav-item nav-link">
+                            Home
+                                </Link>
+                        <Link
+                            to="/about"
+                            className="nav-item nav-link">
+                            About
+                                </Link>
+                        <a
+                            className="nav-item nav-link"
+                            href="http://katesohng.com/blog"
+                            target="_blank">
+                            Blog
+                                </a>
+                        <a
+                            className="nav-item nav-link"
+                            href="mailto:kheeya7@hotmail.com">
+                            Contact
+                                </a>
+                    </div>
+                </div>
+            </Navbar>
         )
     }
 }
